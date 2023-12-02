11c8dd0144febb21dd488a06fe62a7cedff46a3c
janithPet <jpetangoda@gmail.com>
2023-12-02 16:21:23 +0000

    better types for day1 part1

--- 2023/day1/lib/part1.ml
+++ 2023/day1/lib/part1.ml
@@ -3,10 +3,11 @@ open Core
 let explode_string s = List.init (String.length s) ~f:(String.get s)
 let is_digit = function '0' .. '9' -> true | _ -> false
 
-type state = { first : int; last : int }
+type value = None | Digit of int
+type state = { first : value; last : value }
 
-let empty_state () = { first = 0; last = 0 }
-let set_state first last = { first; last }
+let empty_state () = { first = None; last = None }
+let new_state first last = { first; last }
 
 let solve f =
   let lines = In_channel.read_lines f in
@@ -17,11 +18,19 @@ let solve f =
           List.fold_left chars ~init:(empty_state ()) ~f:(fun (acc : state) c ->
               if is_digit c then
                 let c_int = Base.Int.of_string (String.make 1 c) in
-                if acc.first = 0 then set_state c_int 0
-                else set_state acc.first c_int
+                match acc.first with
+                | None -> new_state (Digit c_int) None
+                | Digit _ -> new_state acc.first (Digit c_int)
               else acc)
         in
-        if state.last = 0 then acc + (state.first * 10) + state.first
-        else acc + (state.first * 10) + state.last)
+        match state.last with
+        | None -> (
+            match state.first with
+            | None -> acc
+            | Digit fst -> acc + (fst * 10) + fst)
+        | Digit lst -> (
+            match state.first with
+            | None -> acc
+            | Digit fst -> acc + (fst * 10) + lst))
   in
   string_of_int sum
